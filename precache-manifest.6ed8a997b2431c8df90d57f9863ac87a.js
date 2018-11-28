49b51352e9dfb59106444adcf8fa9ddb01fcceef
Tuan Pham <pham.thanh.tuan@framgia.com>
2018-11-28 15:44:11 +0700

    Updates

---
+++ precache-manifest.6ed8a997b2431c8df90d57f9863ac87a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f849e17378ee4ea4092",
+    "url": "/react-todo-demo/static/js/runtime~main.5f849e17.js"
+  },
+  {
+    "revision": "f1a64f432cd1f6954f9f",
+    "url": "/react-todo-demo/static/js/main.f1a64f43.chunk.js"
+  },
+  {
+    "revision": "6db1c1863fb52f9a6378",
+    "url": "/react-todo-demo/static/js/1.6db1c186.chunk.js"
+  },
+  {
+    "revision": "f1a64f432cd1f6954f9f",
+    "url": "/react-todo-demo/static/css/main.9ab3a1e4.chunk.css"
+  },
+  {
+    "revision": "c4bb670a9ea1caede1cbc12ebfb31372",
+    "url": "/react-todo-demo/index.html"
+  }
+];
